3d4b5e17833c17f891d320f7df825e0bb89e316b
Tarun Sai k <tarunsaikolaa@gmail.com>
2025-12-10 17:38:47 +0530

    Configure backend: Clerk, Cloudinary, Inngest, and env updates

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,33 @@
 import express from "express";
-
+import path from "path";
+import { env } from "./config/env.js";
+import { connectDB } from "./config/db.js";
+import { clerkMiddleware, rootAuthLoader } from '@clerk/react-router/server'
 const app = express();
 
-app.get("/api/health",(req,res)=>{
-    res.status(200).json({message:"ok"});
+const __dirname = path.resolve();
+
+app.use(clerkMiddleware());
+
+app.get("/api/health", (req, res) => {
+     
+    res.status(200).json({ message: "ok" });
 });
 
+// MAKE OUR APP FOR THE PRODUCTION
+if (env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../admin/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
+    });
+}
 
 
-app.listen(3000, () => {
+const server = app.listen(env.PORT, () => {
     console.log("Server is running on port 3000");
+    connectDB();
 });
+
+server();
+
+export default server;
